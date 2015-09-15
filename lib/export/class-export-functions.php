8fde2a32ee8ccfce2b7b8c83ca91d6f314a7179b
Sergio P.A <info@dsergio.com>
2015-09-15 19:29:24 +0200

    change parse args, some errors in various functions

--- lib/export/class-export-functions.php
+++ lib/export/class-export-functions.php
@@ -41,15 +41,16 @@ class Export_Functions extends Export_Base {
 		$arguments = \WP_Parser\get_arguments();
 		wp_reset_postdata();
 		$arguments = apply_filters( 'sublime_parse_function_args', $arguments, $post->post_title );
+
 		return array(
 			'trigger'   => $post->post_title . "\tWP Function",
-			'contents'  => $this->contents( $post->post_title, $this->parse_args( $arguments ), end( $arguments ) ),
+			'contents'  => $this->contents( $post->post_title, $this->parse_args( $arguments ) ),
 			// 'parse'     => $this->parse_args( $arguments ),
 			// 'arguments' => $arguments,
 		);
 	}
 
-	public function contents( $name, $arguments, $last_argument ) {
+	public function contents( $name, $arguments ) {
 		if ( empty( $arguments ) ) {
 			if ( 0 === strpos( $name, '__return_' ) )
 				return apply_filters( 'sublime_parse_function', sprintf( '%s${1:();}', $name ), $name, $arguments );
@@ -57,10 +58,10 @@ class Export_Functions extends Export_Base {
 			return apply_filters( 'sublime_parse_function', sprintf( '%s();', $name ), $name, $arguments );
 		}
 
-		return $this->parse_contents( $arguments, $name, $last_argument );
+		return $this->parse_contents( $arguments, $name );
 	}
 
-	public function parse_contents( $data, $name, $last_argument ) {
+	public function parse_contents( $data, $name ) {
 		if ( 1 === count( $data ) && ! isset( $data[0]['childrens'] ) ) {
 			if ( false === strpos( $data[0]['name'], 'deprecated' ) ) {
 				if ( isset( $data[0]['default_value'] ) ) {
@@ -72,20 +73,15 @@ class Export_Functions extends Export_Base {
 				$arguments = '';
 			}
 		} else {
-			if ( isset( $data[0]['childrens'] ) ) {
-				$arguments = '${1: ';
-				$index = 2;
-			} else {
-				$arguments = ' ';
-				$index = 1;
-			}
+			$arguments = isset( $data[0]['childrens'] ) ? '${1: ' : ' ';
+
+			$index = 1;
 			foreach ( $data as $key => $arg ) {
-				$arguments = $this->argument( $arguments, $arg, $index, 0, isset( $data[0]['childrens'] ), $last_argument );
+				$arguments = $this->argument( $arguments, $arg, $index );
 				$index++;
 			}
 
-			if ( ! isset( $data[0]['childrens'] ) )
-				$arguments .= ' ';
+			$arguments .= isset( $data[0]['childrens'] ) ? ' }' : ' ';
 		}
 
 		$arguments = apply_filters( 'sublime_parse_function_contents', $arguments, $name, $data );
@@ -93,49 +89,38 @@ class Export_Functions extends Export_Base {
 		return apply_filters( 'sublime_parse_function', sprintf( '%s(%s);', $name, $arguments ), $name, $arguments );
 	}
 
-	public function argument( $arguments, $data, $index, $repeat = 0, $wrap = false, $last_argument ) {
-		$name          = isset( $data['default_value'] ) ? $data['default_value'] : sprintf( '\\%s', $data['name'] );
-		$first_element = $wrap ? 2 : 1;
+	public function argument( $arguments, $data, $index ) {
+		$name = isset( $data['default_value'] ) ? $data['default_value'] : sprintf( '\\%s', $data['name'] );
 
 		if ( false === stripos( $data['name'], 'deprecated' ) ) {
 			if ( isset( $data['childrens'] ) ) {
-				if ( $first_element !== $index ) {
-					$arguments .= sprintf( '${%d:, ${%d:%s}', $index, ++$index, $name );
+				if ( '${1: ' === $arguments ) {
+					$arguments .= sprintf( '${%d:%s}', ++$index, $name );
 				} else {
-					$arguments .= sprintf( '${%d:%s}', $index, $name );
+					$arguments .= sprintf( '${%d:, ${%d:%s}', ++$index, ++$index, $name );
 				}
 			} else {
-				if ( $first_element !== $index ) {
-					if ( isset( $data['default_value'] ) ) {
-						$arguments .= sprintf( '${%d:, ${%d:%s}}', $index, ++$index, $name );
+				if ( ' ' === $arguments ) {
+					$arguments .= sprintf( '${%d:%s}', $index, $name );
+				} else {
+					if ( $this->is_optional( $data ) ) {
+						$arguments .= sprintf( '${%d:, ${%d:%s}}', ++$index, ++$index, $name );
 					} else {
 						$arguments .= sprintf( ', ${%d:%s}', $index, $name );
 					}
-				} else {
-					$arguments .= sprintf( '${%d:%s}', $index, $name );
 				}
 			}
 		} else {
-			if ( $data['name'] !== $last_argument['name'] ) {
-				$arguments .= sprintf( '%s%s', ( $first_element !== $index ? ', ' : '' ), $name );
-				++$index;
-			}
+			$arguments .= sprintf( '%s%s', ( preg_match( '/^(\$\{1: | )$/', $arguments ) ? ', ' : '' ), $name );
 		}
 
 		if ( isset( $data['childrens'] ) ) {
-			foreach ( $data['childrens'] as $key => $children ) {
-				$arguments = $this->argument( $arguments, $children, ++$index, ++$repeat, $wrap, $last_argument );
+			foreach ( $data['childrens'] as $children ) {
+				$arguments = $this->argument( $arguments, $children, $index );
+				$index++;
 			}
-		} else {
-			if ( $repeat ) {
-				if ( $wrap && $data['name'] !== $last_argument['name'] ) {
-					$arguments .= str_repeat( '}', $repeat );
-				} elseif ( $wrap ) {
-					$arguments .= ' }';
-				} elseif ( $data['name'] === $last_argument['name'] ) {
-					$arguments .= str_repeat( '}', $repeat );
-				}
-			}
+		} elseif ( $c = preg_match_all('/\$\{([0-9]+):, /', $arguments, $matches, PREG_SET_ORDER ) ) {
+			$arguments .= str_repeat( '}', ( $c - 1 ) );
 		}
 
 		return $arguments;
@@ -143,38 +128,67 @@ class Export_Functions extends Export_Base {
 
 	public function parse_args( $arguments, $args = array() ) {
 		while ( $current = array_shift( $arguments ) ) {
-			$arg = array(
-				'name' => $current['name'],
-			);
+			if ( ! isset( $current['desc'] ) )
+				$current['desc'] = '';
 
-			if ( isset( $current['default_value'] ) )
-				$arg['default_value'] = $current['default_value'];
-
-			if ( isset( $current['childrens'] ) )
-				$arg['childrens'] = $current['childrens'];
-
-			if ( isset( $arg['default_value'] ) && false === stripos( $arg['name'], 'deprecated' ) ) {
+			if ( $this->is_optional( $current ) ) {
 				if ( $last = array_pop( $args ) ) {
-					if ( isset( $last['default_value'] ) ) {
+					if ( $this->is_optional( $last ) ) {
 						if ( isset( $last['childrens'] ) ) {
-							$last['childrens'] = $this->parse_args( array( $arg ), $last['childrens'] );
+							$last['childrens'] = $this->parse_args( array( $current ), $last['childrens'] );
 							$args[] = $last;
 						} else {
-							$last['childrens'][] = $arg;
+							$last['childrens'][] = $current;
 							$args[] = $last;
 						}
 					} else {
 						$args[] = $last;
-						$args[] = $arg;
+						$args[] = $current;
 					}
 				} else {
-					$args[] = $arg;
+					$args[] = $current;
+				}
+			} elseif ( $this->is_deprecated( $current ) ) {
+				if ( $last = array_pop( $args ) ) {
+					if ( isset( $last['childrens'] ) ) {
+						$childrens = $last['childrens'];
+						$last['no_optional'] = true;
+						unset( $last['childrens'] );
+						$args = array_merge( $args, array( $last ), $this->revert_childrens( $childrens ), array( $current ) );
+					}
+				} else {
+					$args[] = $current;
 				}
 			} else {
-				$args[] = $arg;
+				$args[] = $current;
 			}
 		}
 
 		return $args;
+	}
+
+	public function revert_childrens( $childrens ) {
+		$args = array();
+		foreach ( $childrens as $arg ) {
+			if ( isset( $arg['childrens'] ) ) {
+				$x = $arg['childrens'];
+				unset( $arg['childrens'] );
+			}
+			$arg['no_optional'] = true;
+			$args[] = $arg;
+
+			if ( isset( $x ) )
+				$args = array_merge( $args, $this->revert_childrens( $x ) );
+		}
+
+		return $args;
+	}
+
+	public function is_deprecated( $arg ) {
+		return ( false !== stripos( $arg['name'], 'deprecated' ) || 0 === stripos( $arg['desc'], 'deprecated' ) );
+	}
+
+	public function is_optional( $arg ) {
+		return ( ( isset( $arg['default_value'] ) || 0 === stripos( $arg['desc'], 'optional' ) ) && ! $this->is_deprecated( $arg ) && ! isset( $arg['no_optional'] ) );
 	}
 }
