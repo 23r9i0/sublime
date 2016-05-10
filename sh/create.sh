f0fd563f5f95a1de748d5e8b8cbdaf04c93f4dea
23r9i0 <23r9i0@gmail.com>
2016-05-10 11:38:43 +0200

    New linux script

--- sh/create.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for file_sh in update.sh pkg.sh
-do
-	/bin/bash ./$file_sh
-done
--- sh/update.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# create [directory] ( Directory of WordPress ) --user=1 ( Default 1 "user_id". Possible values: user_id or user_name )
-wp subl create ~/www/sublime --user=1
