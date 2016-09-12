dc6e2cc4e81f3156867d8381962623dba38b26af
23r9i0 <23r9i0@gmail.com>
2016-09-13 01:07:50 +0200

    Customize for mayor compatibility of the WordPress Completions

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name: Sublime
  * Plugin URI: https://github.com/23r9i0/sublime
  * Description: Helper to generate sublime text completions
- * Version: 1.1
+ * Version: 2.0
  * Author: Sergio P.A. ( 23r9i0 )
  * Author URI: http://dsergio.com/
  * License: GPLv2 or later
@@ -19,11 +19,18 @@ if ( false !== stream_resolve_include_path( __DIR__ . '/vendor/autoload.php' ) )
 	/**
 	 * Initialize this plugin
 	 */
-	new Sublime\Plugin();
+	new \Sublime\Plugin();
 
-	/**
-	 * Add Config file to apply filters
-	 */
-	if ( false !== stream_resolve_include_path( __DIR__ . '/config.php' ) )
-		require_once __DIR__ . '/config.php';
+	// if ( ! function_exists( 'je' ) ) {
+	// 	function je( $data ) {
+	// 		return json_encode( $data, JSON_PRETTY_PRINT );
+	// 	}
+	// }
+
+	// if ( ! function_exists( 'fpc' ) ) {
+	// 	function fpc( $name, $data, $append = false ) {
+	// 		$ext = ( false !== strpos( $name, '.' ) ? '' : ( is_string( $data ) ? '.txt' : '.json' ) );
+	// 		file_put_contents( __DIR__ . "/{$name}{$ext}", ( is_string( $data ) ? $data : je( $data ) ), ( $append ? FILE_APPEND : 0 ) );
+	// 	}
+	// }
 }
